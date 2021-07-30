b2a57b4726bd6f84b6b8c2c7c5c0c3aa26c5bd97
نايك احمد عياد <83564699+exalex200@users.noreply.github.com>
2021-07-30 21:14:30 +0200

    Add files via upload

--- DRAGON.lua
+++ DRAGON.lua
@@ -14523,7 +14523,7 @@ local msg_id = msg.id_/2097152/0.5
 https.request("https://api.telegram.org/bot"..token..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo=https://t.me/SUL_A_Q&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard)) 
 end
 
-if text == 'مبرمج السورس or text == 'دانيال' then  
+if text == 'مبرمج السورس 'or text == 'دانيال' then  
 local Text = [[
 
 المبرمج دانيال لو حابب تواصل معاه 
